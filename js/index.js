75151b80d0b5fe1eb4f4adba7f2b9f4d1801a523
unknown <flaming.june123@gmail.com>
2023-07-09 18:57:04 +0900

    20230709_1857

--- js/index.js
+++ js/index.js
@@ -1,16 +1,21 @@
 const dayOfWeek = ['日','月','火','水','木','金','土'];
-const week = [1,2,3,4,5,6,7];
 
+const calender = document.querySelector('.p-calender');
 
-//v-forを使って実装出来ないか
-Vue.createApp({
-    data(){
-        return{
-            week1:[
-                {
-                    day:i
-                }
-            ]
-        }
+for(let i = 0; i < 31; i++){
+    //日の要素を追加
+    let dayContent = document.createElement('div');
+    dayContent.classList.add('p-day-content');
+    if(i === 0 || (i % 7) === 0){
+        dayContent.classList.add('u-sunday');
     }
-})
+    dayContent.textContent = i + 1;
+    calender.appendChild(dayContent);
+    
+    if((i % 7) === 6){
+            let createBr = document.createElement('br');
+            dayContent.after(createBr);
+        }
+    };
+    
+//カレンダーに週を追加
