2c1d5631817ca9243a5c57ed4860f233e20138e9
naruiyamazaki <flaming.june123@gmail.com>
2023-07-09 13:53:32 +0900

    20230709_1353

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const dayOfWeek = ['日','月','火','水','木','金','土'];
+const week = [1,2,3,4,5,6,7];
+
+
+//v-forを使って実装出来ないか
+Vue.createApp({
+    data(){
+        return{
+            week1:[
+                {
+                    day:i
+                }
+            ]
+        }
+    }
+})
